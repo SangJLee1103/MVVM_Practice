5be870622477db26cb55edddf9065298c928fe22
sangjun <jesusfollow1103@naver.com>
2022-09-09 22:29:57 +0900

    feat: 네트워크 추가(UrlSession -> Alamofire 적용)

--- MVVM_Practice/Services/WebService.swift
+++ MVVM_Practice/Services/WebService.swift
@@ -6,22 +6,40 @@
 //
 
 import Foundation
+import Alamofire
 
 class WebService {
-    func getArticles(url: URL, completion:@escaping([Article]?) -> ()) {
-        URLSession.shared.dataTask(with: url) {(data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                completion(nil)
-            } else if let data = data {
-                let articleList = try? JSONDecoder().decode(ArticleList.self, from: data)
-                print(articleList)
-                
-                if let articleList = articleList {
-                    completion(articleList.articles)
+//    func getArticles(url: URL, completion:@escaping([Article]?) -> ()) {
+//        URLSession.shared.dataTask(with: url) {(data, response, error) in
+//            if let error = error {
+//                print(error.localizedDescription)
+//                completion(nil)
+//            } else if let data = data {
+//                let articleList = try? JSONDecoder().decode(ArticleList.self, from: data)
+//                print(articleList)
+//
+//                if let articleList = articleList {
+//                    completion(articleList.articles)
+//                }
+//                print(articleList?.articles)
+//            }
+//        }.resume()
+//    }
+//
+    
+    // Alamofire 적용
+    func getArticles(url: URL, completion: @escaping([Article]?) ->()) {
+        AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil)
+            .validate()
+            .responseDecodable(of: ArticleList.self) { (response) in
+                switch response.result {
+                case .success(let response):
+                    completion(response.articles)
+                    print(response.articles)
+                case .failure(let error):
+                    completion(nil)
+                    print(error.localizedDescription)
                 }
-                print(articleList?.articles)
             }
-        }.resume()
     }
 }
